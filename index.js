220777cf0492dd79fd4f3927a3abc82a63d7251f
Mitch Allen <github@mitchallen.com>
2025-09-16 07:28:16 -0400

    feat: use ES modules instead of CommonJS

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-const main = () => {
+export const main = () => {
   return "Hello, World!";
 }
 
 console.log(main());
-
-module.exports = {
-  main
-};
--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-const { main } = require('./index');
+import { main } from './index.js';
 
 test('should return "Hello, World!"', () => {
   expect(main()).toBe('Hello, World!');
